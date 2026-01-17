ef50f0338e368796a6fc2c8b73e78e6899ca579f
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-01-17 23:06:43 +0530

    m

---
+++ Ecommerces.sherd/Interface/IGenericInterface.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerces.sherd.Responses;
+using System.Linq.Expressions;
+
+
+namespace Ecommerces.sherd.Interface
+{
+    public interface IGenericInterface<T> where T : class
+    {
+        Task<Response> CreateAsync(T entity);
+        Task<Response> UpdateAsync(T entity);
+        Task<Response> DeleteAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetByAsync(Expression<Func<T, bool>> expression);
+
+
+    }
+}
