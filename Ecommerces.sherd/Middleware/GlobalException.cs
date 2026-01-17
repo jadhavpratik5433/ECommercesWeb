15533f90de769c019313e1f08b614e3342c3d7e6
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-01-17 23:07:24 +0530

    as

---
+++ Ecommerces.sherd/Middleware/GlobalException.cs
@@ -0,0 +1,78 @@
+﻿using Ecommerces.sherd.Logs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace Ecommerces.sherd.Middleware
+{
+    public class GlobalException(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string message = "sorry internal server error occurred";
+            int statusCode = (int)HttpStatusCode.InternalServerError;
+            string title = "error";
+
+            try
+            {
+                await next(context);
+                if (context.Response.StatusCode == StatusCodes.Status429TooManyRequests)
+                {
+                    title = "not found";
+                    message = "too meany request made";
+                    statusCode = (int)StatusCodes.Status429TooManyRequests;
+                    await ModifyHeader(context, statusCode, title, message);
+
+                }
+                if(context.Response.StatusCode == StatusCodes.Status401Unauthorized)
+                {
+                    title = "Alert";
+                    message = "you are not authorized person";
+                    await ModifyHeader(context, statusCode, title, message);
+                }
+                if(context.Response.StatusCode == StatusCodes.Status403Forbidden)
+                {
+                    title = "uot of access";
+                    message = "you are not allowed";
+                    statusCode = StatusCodes.Status403Forbidden;
+                    await ModifyHeader(context, statusCode, title, message);
+                }
+
+            }
+            catch(Exception ex)
+            {
+                LogExpception.LogExpceptions(ex);
+                if(ex is TaskCanceledException || ex is TimeoutException)
+                {
+                    title = "out of time";
+                    message = "Request to time out ... try again";
+                    statusCode = StatusCodes.Status408RequestTimeout;
+                }
+                await ModifyHeader(context, statusCode, title, message);
+            }
+        
+        }
+            
+    
+
+        private static async Task ModifyHeader(HttpContext context, int statusCode, string title, string message)
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(new ProblemDetails() { 
+            
+                Detail = message,
+                Title = title,
+
+                Status = statusCode
+
+            }), CancellationToken.None);
+            return;
+
+        }
+    }
+}
---
+++ Ecommerces.sherd/Middleware/ListenToOnlyApiGetaway.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerces.sherd.Middleware
+{
+    public class ListenToOnlyApiGetaway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var signedHeader = context.Request.Headers["Api-Getaway"];
+
+            if(signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("sorry service is unavailable");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
