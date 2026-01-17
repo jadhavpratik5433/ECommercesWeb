e4c64f25ab08f8c7e36d610039f3f53b899940e1
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-01-17 23:06:15 +0530

    new change

---
+++ Ecommerces.sherd/DependanceInjection/SharedServiceContainer.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerces.sherd.DependanceInjection
+{
+    public static class SharedServiceContainer
+    {
+        public static IServiceCollection AddSharedServices<TContext>
+            (this IServiceCollection services,IConfiguration config, string FileName) where TContext : DbContext
+           
+        {
+            // Register shared services here
+            // services.AddScoped<YourSharedService>();
+            return services;
+        }
+    }
+}
---
+++ Ecommerces.sherd/Logs/LogExpception.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Serilog;
+
+namespace Ecommerces.sherd.Logs
+{
+    public static class LogExpception
+    {
+        public static void LogExpceptions(Exception ex)
+        {
+            LogToFile(ex.Message);
+            LogToConsole(ex.Message);
+            LogToDebugger(ex.Message);
+        }
+        public static void LogToFile(string message) => Log.Information(message);
+        public static void LogToConsole(string message) => Log.Information(message);
+        public static void LogToDebugger(string message) => Log.Information(message);
+
+
+        // Implementation for logging to a file
+
+    }
+}
